02cd99e2a0e26431e4f2812a21560695b328363c
Mt1999 <1844762507@qq.com>
2019-08-16 00:26:54 +0800

    refactor:标签功能未完成

--- src/components/SqHeader.vue
+++ src/components/SqHeader.vue
@@ -5,20 +5,24 @@
         角色：<span>总代</span> 
       <i></i>退出登录<i></i>
       选择语言：
-      <div>
-        <input type="text" class="input" >
-        <ul>
-          <li></li>
-        </ul>
-      </div>
+      <el-select v-model="value" placeholder="请选择" size="mini" >
+        <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        >
+        </el-option>
+      </el-select>
     </div>
     <div class="hd-bot">
       <ul>
         <li>在线用户</li>
-        <li>在线用户</li>
-        <li>在线用户</li>
-        <li>在线用户</li>
-        <li>在线用户</li>
+        <li>游戏记录</li>
+        <li>游戏报表</li>
+        <li>用户管理</li>
+        <li>我的面板</li>
+        <li>系统日志</li>
       </ul>
     </div>
   </div>
@@ -30,19 +34,19 @@ export default {
       return {
         options: [{
           value: '选项1',
-          label: '黄金糕'
+          label: '简体中文'
         }, {
           value: '选项2',
-          label: '双皮奶'
+          label: '繁体中文'
         }, {
           value: '选项3',
-          label: '蚵仔煎'
+          label: '英文'
         }, {
           value: '选项4',
-          label: '龙须面'
+          label: '日文'
         }, {
           value: '选项5',
-          label: '北京烤鸭'
+          label: '韩国'
         }],
         value: ''
       }
@@ -83,16 +87,39 @@ export default {
 }
 .el-select .el-input {
   width: 130px;
+  height: 14px;
 }
-.select-input {
+.el-select>.el-input {
+  display: block;
+  height: 14px;
+}
+>>> .el-input__inner {
+  height: 18px;
+}
+.input__inner {
   height: 18px;
 }
 .input-with-select .el-input-group__prepend {
   background-color: #fff;
 }
 
-
 .header-right .hd-bot {
   height: 30px;
+}
+.header-right .hd-bot ul{
+  height: 30px;
+  margin: 0;
+  display: flex;
+  justify-content: space-between;
+}
+.header-right .hd-bot ul li{
+  height: 26px;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 12px;
+  color: #fff;
+  border: 1px solid red;
 }
 </style>
